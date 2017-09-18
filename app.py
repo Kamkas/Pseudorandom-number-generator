c3cfb1c990f3420a365ca63b7dd6cdda599e8fc5
IvanSerditov <john.serditof@yandex.ru>
2017-09-18 23:38:57 +0400

    added keyboard interrupt. park-miller rework.
    refactoring

---
+++ app.py
@@ -0,0 +1,5 @@
+import sys
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import GLib, Gio, Gtk, GObject
--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import random
 import sys
+
 from numpy import random as rnd
 
 
@@ -18,27 +19,29 @@ class Generator:
         filled_len = int(round(bar_len * count / float(total)))
         percents = round(100.0 * count / float(total), 1)
         bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
         sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
         sys.stdout.flush()
 
     def python_builtin_rand(self, bytes_number):
-        print("Starting python builtin pseudo-generator...")
-        random.seed(self.seed, version=2)
-        arr = list()
-        for index in range(bytes_number):
-            arr.append(random.randint(0, 1))
-            self.progress_bar(index, bytes_number)
-        print("Done!")
-        return arr
+        try:
+            print("Starting python builtin pseudo-generator...")
+            random.seed(self.seed, version=2)
+            arr = list()
+            for index in range(bytes_number):
+                arr.append(random.randint(0, 1))
+                self.progress_bar(index, bytes_number)
+            print("Done!")
+            return arr
+        except KeyboardInterrupt:
+            print("\nCanceled by user.")
+            return list()
 
     def numpy_builtin_rand(self, bytes_number):
-        print("Starting NumPy builtin pseudo-generator...")
-        # random.seed(self.seed)
-        r = rnd.RandomState()
-        r.seed(self.seed)
-        arr = list()
         try:
+            print("Starting NumPy builtin pseudo-generator...")
+            r = rnd.RandomState()
+            r.seed(self.seed)
+            arr = list()
             for index in range(bytes_number):
                 arr.append(r.choice((0, 1)))
                 self.progress_bar(index, bytes_number)
@@ -46,66 +49,79 @@ class Generator:
             return arr
         except KeyboardInterrupt:
             print("\nCanceled by user.")
-            return arr
+            return list()
 
     def fibo_lagged_rand(self, bytes_number):
-        arr = list()
-        r = random.Random()
-        print("Please enter what (a,b) to use:\n [1] (17,5)\n [2] (55,24)\n [3] (97,33)\n")
-        choice = input("Choice: ")
-        if choice is "1":
-            (a, b) = (17, 5)
-        elif choice is "2":
-            (a, b) = (55, 24)
-        elif choice is "3":
-            (a, b) = (97, 33)
-        else:
-            print("Wrong choice")
-            return arr
-        if bytes_number >= a:
-            print("\nStarting python builtin pseudo-generator...")
+        try:
+            r = random.Random()
             r.seed(self.seed)
-            for index in range(a):
-                arr.append(r.uniform(0.0, 1.0))
-                self.progress_bar(index + 1, a)
+            print("Please enter lags (a,b) what to use:\n [1]: (17,5)\n [2]: (55,24)\n [3]: (97,33)\n")
+            choice = input("Choice: ")
+            if choice is "1":
+                (lag_a, lag_b) = (17, 5)
+            elif choice is "2":
+                (lag_a, lag_b) = (55, 24)
+            elif choice is "3":
+                (lag_a, lag_b) = (97, 33)
+            else:
+                print("Wrong choice")
+                return list()
+            print("\nStarting python builtin pseudo-generator for initial sequence...")
+            arr = [r.uniform(0, 1) for i in range(lag_a + 1)]
             print("\nDone!\nStarting fibo pseudo-generator...")
-            for index in range(a + 1, bytes_number, 1):
-                self.progress_bar(index - a, bytes_number - a)
-                if arr[index - a] >= arr[index - b]:
-                    arr.append(arr[index - a] - arr[index - b])
-                elif arr[index - a] < arr[index - b]:
-                    arr.append(arr[index - a] - arr[index - b])
-                else:
-                    print("generator error occured!")
-                    break
+            for index in range(lag_a + 2, bytes_number + 1):
+                x_k = arr[index - lag_a] - arr[index - lag_b]
+                if x_k < 0:
+                    x_k += 1
+                arr.append(x_k)
+                self.progress_bar(index - lag_a, bytes_number - lag_a)
+            print("\nDone!\nChanging values from float to binary...")
             for index, x in enumerate(arr):
+                self.progress_bar(index, bytes_number)
                 if x < 0.5:
                     arr[index] = 0
                 else:
                     arr[index] = 1
-            print("Done!")
+            print("\nDone!")
             return arr
+        except KeyboardInterrupt:
+            print("\nCanceled by user.")
+            return list()
+
+    def pm_rand_round(self, last_value):
+        IA = 16807
+        IM = 2147483647
+
+        next_value = 0
+
+        if last_value is not None or last_value is not 0:
+            next_value = IA * last_value % IM
+        else:
+            raise ValueError("Value should be >= 1")
+
+        return next_value
 
     def pm_rand(self, bytes_number):
-        IA = 7 ** 5
-        IM = 2 ** 31 - 1
-        AM = 1.0 / IM
-
-        IQ = 12773
-        IR = 2836
-
-        MASK = 123456789
-
-        seed = self.seed
-
-        seed ^= MASK
-        k = seed / IQ
-        seed = IA * (seed - k * IQ) - IR * k
-        if seed < 0:
-            seed += IM
-        ans = AM * seed
-        seed ^= MASK
-        return ans
+        try:
+            arr = list()
+            print("\nStarting parka-miller's pseudo-generator...")
+            for index in range(bytes_number):
+                if index is 0:
+                    arr.append(self.pm_rand_round(self.seed))
+                arr.append(self.pm_rand_round(arr[index - 1]))
+                self.progress_bar(index, bytes_number)
+            print("\nDone!\nStarting convert values from integer to binary...")
+            bin_arr = list()
+            a = (2 ** 31 - 1) // 2
+            for index, el in enumerate(arr):
+                if el < a + 1:
+                    bin_arr.append(0)
+                elif el >= a:
+                    bin_arr.append(1)
+            return bin_arr
+        except KeyboardInterrupt:
+            print("\nCanceled by user.")
+            return list()
 
     def write_to_file(self, arr):
         print("Writing to file \"%s\"" % self.output_filename)
@@ -113,11 +129,3 @@ class Generator:
             f.write(''.join([str(x) for x in arr]))
             f.close()
         print("Done!")
-
-
-if __name__ == '__main__':
-    g = Generator(None, None)
-    # print(g.python_builtin_rand(5))
-    # print(g.numpy_builtin_rand(5))
-    print(g.seed)
-    g.python_builtin_rand(100000)
